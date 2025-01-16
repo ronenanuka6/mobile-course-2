17d810bbf3bf38c534d8be6cc1b71ff44e8c1121
ronenanuka6 <ronenanuka6@gmail.com>
2025-01-16 18:00:58 +0200

    added student list

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.assignment2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.student_list_activity_list_view)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val addStudentButton: Button = findViewById(R.id.main_activity_add_student_button)
+        addStudentButton.setOnClickListener {
+            val intent = Intent(this, AddStudentActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/assignment2/Student.kt
+++ app/src/main/java/com/example/assignment2/Student.kt
@@ -1,7 +1,5 @@
 package com.example.assignment2
 
-//class Student {
-//}
 data class Student(
     var id: String,
     var name: String,
